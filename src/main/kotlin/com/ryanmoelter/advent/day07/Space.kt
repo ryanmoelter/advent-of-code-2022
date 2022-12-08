37d48fde402a7a55df73773b98b5d95452656396
Ryan Moelter <hello@ryanmoelter.com>
2022-12-07 21:51:32 -0800

    Get ready for day 7

---
+++ src/main/kotlin/com/ryanmoelter/advent/day07/Space.kt
@@ -0,0 +1,9 @@
+package com.ryanmoelter.advent.day07
+
+fun main() {
+  println(findDirectoriesUnder100K(spaceData))
+}
+
+fun findDirectoriesUnder100K(input: String): Int {
+  return 0
+}
---
+++ src/test/kotlin/com/ryanmoelter/advent/day07/SpaceTest.kt
@@ -0,0 +1,38 @@
+package com.ryanmoelter.advent.day07
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.ints.shouldBeExactly
+
+class SpaceTest : FunSpec({
+  context("example should work") {
+    val input = """
+      ${'$'} cd /
+      ${'$'} ls
+      dir a
+      14848514 b.txt
+      8504156 c.dat
+      dir d
+      ${'$'} cd a
+      ${'$'} ls
+      dir e
+      29116 f
+      2557 g
+      62596 h.lst
+      ${'$'} cd e
+      ${'$'} ls
+      584 i
+      ${'$'} cd ..
+      ${'$'} cd ..
+      ${'$'} cd d
+      ${'$'} ls
+      4060174 j
+      8033020 d.log
+      5626152 d.ext
+      7214296 k
+    """.trimIndent()
+
+    test("for part 1") {
+      findDirectoriesUnder100K(input) shouldBeExactly 2
+    }
+  }
+})
